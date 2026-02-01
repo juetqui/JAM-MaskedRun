02dd8f81e12bebb07300732e81522ed63fae652e
matiasbenavide <matiasbenavide@gmail.com>
2026-02-01 16:31:41 -0300

    Vuelvo a agregar cambio de skybox en EnvironmentChanger

--- Assets/Scripts/EnvironmentChanger.cs
+++ Assets/Scripts/EnvironmentChanger.cs
@@ -3,23 +3,27 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class EnvironmentChanger : MonoBehaviour
-{   
+{
+    [SerializeField] private Material forestMaterial;
+    [SerializeField] private Material fireMaterial;
+
+    private Material _targetMaterial;
     private WorldType _currentWorld = WorldType.Forest;
     private WorldType _targetWorld = WorldType.Fire;
 
     private void Start()
     {
         PlayerLaneMovement.Instance.OnWorldChanged += UpdateWorldType;
+        _targetMaterial = fireMaterial;
     }
 
     private void UpdateWorldType()
     {
-        // RenderSettings.ambientSkyColor = skyColor;
-        // RenderSettings.ambientEquatorColor = equatorColor;
-        // RenderSettings.ambientGroundColor = groundColor;
+        RenderSettings.skybox = _targetMaterial;
         
         _currentWorld = _targetWorld;
         _targetWorld = _currentWorld == WorldType.Forest ? WorldType.Fire : WorldType.Forest;
+        _targetMaterial = _currentWorld == WorldType.Forest ? fireMaterial : forestMaterial;
     }
 
     private enum WorldType
