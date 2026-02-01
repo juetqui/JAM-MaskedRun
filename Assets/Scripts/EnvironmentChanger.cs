05a24fb0cf4c728602deb68fdd56b1b3fc827649
matiasbenavide <matiasbenavide@gmail.com>
2026-02-01 14:18:55 -0300

    Modifico cambio de skybox por gradiente

--- Assets/Scripts/EnvironmentChanger.cs
+++ Assets/Scripts/EnvironmentChanger.cs
@@ -3,12 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class EnvironmentChanger : MonoBehaviour
-{
-    [SerializeField] private Material forestSkybox;
-    [SerializeField] private Material fireSkybox;
-
-    private Material _targetSkyBox;
-    
+{   
     private WorldType _currentWorld = WorldType.Forest;
     private WorldType _targetWorld = WorldType.Fire;
 
@@ -19,11 +14,12 @@ public class EnvironmentChanger : MonoBehaviour
 
     private void UpdateWorldType()
     {
-        RenderSettings.skybox = _targetSkyBox;
+        // RenderSettings.ambientSkyColor = skyColor;
+        // RenderSettings.ambientEquatorColor = equatorColor;
+        // RenderSettings.ambientGroundColor = groundColor;
         
         _currentWorld = _targetWorld;
         _targetWorld = _currentWorld == WorldType.Forest ? WorldType.Fire : WorldType.Forest;
-        _targetSkyBox = _currentWorld == WorldType.Forest ? fireSkybox : forestSkybox;
     }
 
     private enum WorldType
